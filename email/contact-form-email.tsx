baeadbbf986361819b8f93576a4d001581e48419
Ben Hur <bhur777@gmail.com>
2023-08-25 06:15:16 -0400

    import react-email

---
+++ email/contact-form-email.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {
+	Html,
+	Body,
+	Head,
+	Heading,
+	Hr,
+	Container,
+	Preview,
+	Section,
+	Text,
+} from "@react-email/components";
+import { Tailwind } from "@react-email/tailwind";
+
+export default function ContactFormEmail() {
+	return <div>contact-form-email</div>;
+}
