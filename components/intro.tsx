8635b4f6be7bf47a68ff25191277c2e96111c4e5
Ben Hur <bhur777@gmail.com>
2023-08-25 01:49:15 -0400

    Add h1 and configure eslintrc.json

--- components/intro.tsx
+++ components/intro.tsx
@@ -41,6 +41,19 @@ export default function Intro() {
 					</motion.span>
 				</div>
 			</div>
+			<motion.h1
+				className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+				initial={{ opacity: 0, y: 100 }}
+				animate={{ opacity: 1, y: 0 }}
+			>
+				<span className="font-bold">Hello, I'm Ben.</span> I'm a{" "}
+				<span className="font-bold">
+					Front-end focused developer with full-stack experience as well
+				</span>{" "}
+				with <span className="font-bold">1 year</span> of experience. I enjoy
+				building <span className="italic">sites & apps</span>. My focus is{" "}
+				<span className="underline">React (Next.js)</span>.
+			</motion.h1>
 		</section>
 	);
 }
