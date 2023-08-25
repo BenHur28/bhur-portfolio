3cc6c7ac298a4eddbb6cf0097ec76bca2aa05c64
Ben Hur <bhur777@gmail.com>
2023-08-25 01:13:16 -0400

    Animate header

--- components/header.tsx
+++ components/header.tsx
@@ -1,15 +1,20 @@
+"use client";
+
 import React from "react";
+import { motion } from "framer-motion";
 
 export default function Header() {
 	return (
 		<header className="z-100 relative">
-			<div
-				className="fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-full rounded-none border border-opacity-40
+			<motion.div
+				className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-opacity-40
 			 	bg-white bg-opacity-80 shadow-gl shadow-black/[0.03] backdrop-blur-[0.5rem] 
 			 	sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full"
+				initial={{ y: -100, x: "-50%", opacity: 0 }}
+				animate={{ y: 0, x: "-50%", opacity: 1 }}
 			>
 				<div></div>
-			</div>
+			</motion.div>
 		</header>
 	);
 }
