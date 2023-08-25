7a79b470c93b4bbc0e9ad46c58476442c356a9cb
Ben Hur <bhur777@gmail.com>
2023-08-25 02:02:14 -0400

    Create cv/github/linkedin buttons/links

--- components/intro.tsx
+++ components/intro.tsx
@@ -3,7 +3,9 @@
 import { motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
-import { BsArrowRight } from "react-icons/bs";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
 import React from "react";
 
 export default function Intro() {
@@ -56,14 +58,44 @@ export default function Intro() {
 				building <span className="italic">sites & apps</span>. My focus is{" "}
 				<span className="underline">React (Next.js)</span>.
 			</motion.h1>
-			<div>
+			<div className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium">
 				<Link
 					href="#contact"
 					className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none 
                     focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
 				>
-					Contact me here <BsArrowRight />
+					Contact me here{" "}
+					<BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
 				</Link>
+
+				<a
+					className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none 
+                    focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+					href="/"
+					download
+				>
+					Download CV{" "}
+					<HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+				</a>
+				<a
+					className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full 
+                    focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer 
+                    borderBlack dark:bg-white/10 dark:text-white/60"
+					href="https://www.linkedin.com/in/benhur93/"
+					target="_blank"
+				>
+					<BsLinkedin />
+				</a>
+
+				<a
+					className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full 
+                    focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer 
+                    borderBlack dark:bg-white/10 dark:text-white/60"
+					href="https://github.com/BenHur28"
+					target="_blank"
+				>
+					<FaGithubSquare />
+				</a>
 			</div>
 		</section>
 	);
