071843efec11a7ffa5141c9133cb51204615c718
Ben Hur <bhur777@gmail.com>
2023-08-25 01:21:40 -0400

    create navbar links

--- components/header.tsx
+++ components/header.tsx
@@ -2,6 +2,8 @@
 
 import React from "react";
 import { motion } from "framer-motion";
+import { links } from "@/lib/data";
+import Link from "next/link";
 
 export default function Header() {
 	return (
@@ -17,7 +19,20 @@ export default function Header() {
 				<ul
 					className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 
 				sm:w-[initial] sm:flex-nowrap sm:gap-5"
-				></ul>
+				>
+					{links.map((link) => (
+						<motion.li
+							className="h-3/4 flex items-center justify-center relative"
+							key={link.name}
+							initial={{ y: -100, opacity: 0 }}
+							animate={{ y: 0, opacity: 1 }}
+						>
+							<Link className="" href={link.hash}>
+								{link.name}
+							</Link>
+						</motion.li>
+					))}
+				</ul>
 			</nav>
 		</header>
 	);
