78196f6349350a61075e542cdefe732faad461d4
Ben Hur <bhur777@gmail.com>
2023-08-25 04:43:52 -0400

    add experience to main page

--- components/experience.tsx
+++ components/experience.tsx
@@ -1,5 +1,22 @@
+"use client";
+
 import React from "react";
+import SectionHeader from "./section-header";
+import { useSectionInView } from "@/lib/hooks";
+import { experiencesData } from "@/lib/data";
+import {
+	VerticalTimeline,
+	VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
 
 export default function Experience() {
-	return <div>Experience</div>;
+	const { ref } = useSectionInView("Experience");
+
+	return (
+		<section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
+			<SectionHeader>My Experience</SectionHeader>
+			<VerticalTimeline></VerticalTimeline>
+		</section>
+	);
 }
