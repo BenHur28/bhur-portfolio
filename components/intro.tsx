612c87c8b2ef8fa9ba7c8ebbcae531982144e66f
Ben Hur <bhur777@gmail.com>
2023-08-25 01:41:51 -0400

    place emoji

--- components/intro.tsx
+++ components/intro.tsx
@@ -5,8 +5,17 @@ export default function Intro() {
 	return (
 		<section>
 			<div className="flex items-center justify-center">
-				<div>
-					<Image height="192" width="192" src="/pfp.png" alt="Ben Hur" />
+				<div className="relative">
+					<div></div>
+					<Image
+						height={192}
+						width={192}
+						quality={95}
+						src="/pfp.png"
+						alt="Ben Hur"
+						className="h-24 w-24 rounded-full border-[0.35rem] border-white shadow-xl"
+					/>
+					<span className="absolute bottom-0 right-0 text-2xl">👋</span>
 				</div>
 			</div>
 		</section>
