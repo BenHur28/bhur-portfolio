1d59dd9981605ac992c86268a2c6a94f88a0fb2b
Ben Hur <bhur777@gmail.com>
2023-08-25 04:26:57 -0400

    create skills.tsx section

---
+++ components/skills.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import SectionHeader from "./section-header";
+
+export default function Skills() {
+	return (
+		<section id="skills">
+			<SectionHeader>My Skills</SectionHeader>
+		</section>
+	);
+}
