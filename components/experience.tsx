579444d21e2faae63983840927bd1d564211f1ba
Ben Hur <bhur777@gmail.com>
2023-08-25 07:13:00 -0400

    add light/dark theme to experience

--- components/experience.tsx
+++ components/experience.tsx
@@ -9,9 +9,11 @@ import {
 	VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
+import { useTheme } from "@/context/theme-context";
 
 export default function Experience() {
 	const { ref } = useSectionInView("Experience");
+	const { theme } = useTheme();
 
 	return (
 		<section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
@@ -21,7 +23,8 @@ export default function Experience() {
 					<React.Fragment key={index}>
 						<VerticalTimelineElement
 							contentStyle={{
-								background: "#f3f4f6",
+								background:
+									theme === "light" ? "#f3f4f6" : "rgba(255, 255, 255, 0.05)",
 								boxShadow: "none",
 								border: "1px solid rgba(0, 0, 0, 0.05)",
 								textAlign: "left",
@@ -33,7 +36,8 @@ export default function Experience() {
 							date={exp.date}
 							icon={exp.icon}
 							iconStyle={{
-								background: "white",
+								background:
+									theme === "light" ? "white" : "rgba(255, 255, 255, 0.15)",
 								fontSize: "1.5rem",
 							}}
 						>
