65a6e8aa6abb2038fd3cc0d1d55812bd74f11627
Ben Hur <bhur777@gmail.com>
2023-08-25 02:03:22 -0400

    Animate buttons dive

--- components/intro.tsx
+++ components/intro.tsx
@@ -45,6 +45,7 @@ export default function Intro() {
 					</motion.span>
 				</div>
 			</div>
+
 			<motion.h1
 				className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
 				initial={{ opacity: 0, y: 100 }}
@@ -58,7 +59,15 @@ export default function Intro() {
 				building <span className="italic">sites & apps</span>. My focus is{" "}
 				<span className="underline">React (Next.js)</span>.
 			</motion.h1>
-			<div className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium">
+
+			<motion.div
+				className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+				initial={{ opacity: 0, y: 100 }}
+				animate={{ opacity: 1, y: 0 }}
+				transition={{
+					delay: 0.1,
+				}}
+			>
 				<Link
 					href="#contact"
 					className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none 
@@ -96,7 +105,7 @@ export default function Intro() {
 				>
 					<FaGithubSquare />
 				</a>
-			</div>
+			</motion.div>
 		</section>
 	);
 }
