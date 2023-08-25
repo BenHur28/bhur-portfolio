a47a03deda0d37fb9f0e5f8edf8066024a1aad77
Ben Hur <bhur777@gmail.com>
2023-08-25 03:54:40 -0400

    add ref inview hook to intro

--- components/projects.tsx
+++ components/projects.tsx
@@ -8,7 +8,7 @@ import { useActiveSectionContext } from "@/context/active-session-context";
 import { useInView } from "react-intersection-observer";
 
 export default function Projects() {
-	const { ref, inView } = useInView();
+	const { ref, inView } = useInView({ threshold: 0.5 });
 	const { setActiveSection } = useActiveSectionContext();
 
 	useEffect(() => {
